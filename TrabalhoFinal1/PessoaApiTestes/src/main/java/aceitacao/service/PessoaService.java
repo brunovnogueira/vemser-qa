63447d0957c56f18027e92adc542f495b99366d5
Bruno Nogueira <brunovnogueira@gmail.com>
2022-08-08 13:14:35 -0300

    atualizando token

--- TrabalhoFinal1/PessoaApiTestes/src/main/java/aceitacao/service/PessoaService.java
+++ TrabalhoFinal1/PessoaApiTestes/src/main/java/aceitacao/service/PessoaService.java
@@ -10,7 +10,7 @@ public class PessoaService {
 
     public PaginacaoPessoaDTO listarPessoas(Integer pagina, Integer tamanho){
         String url = baseUrl + "pessoa?pagina={pagina}&tamanhoDasPaginas={tamanho}";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         PaginacaoPessoaDTO res = given()
                 .contentType(ContentType.JSON)
@@ -31,8 +31,7 @@ public class PessoaService {
 
     public PessoaDTO listarPorCpf(String cpf){
         String url = baseUrl + "pessoa/{cpf}/cpf";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
-
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         PessoaDTO res = given()
                 .contentType(ContentType.JSON)
@@ -52,8 +51,7 @@ public class PessoaService {
 
     public Erro500DTO listarPorCpfVazio(String cpf){
         String url = baseUrl + "pessoa/{cpf}/cpf";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
-
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         Erro500DTO res = given()
                 .contentType(ContentType.JSON)
@@ -73,7 +71,7 @@ public class PessoaService {
 
     public RelatorioPessoaDTO[] listarRelatorio(String id){
         String url = baseUrl + "pessoa/relatorio?idPessoa={id}";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         RelatorioPessoaDTO[] res = given()
                 .contentType(ContentType.JSON)
@@ -93,7 +91,7 @@ public class PessoaService {
 
     public ListaCompletaDTO[] listarListaCompleta(String id){
         String url = baseUrl + "pessoa/lista-completa?idPessoa={id}";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         ListaCompletaDTO[] res = given()
                 .contentType(ContentType.JSON)
@@ -113,7 +111,7 @@ public class PessoaService {
 
     public ListaComEnderecoDTO[] listarListaComEndereco(String id){
         String url = baseUrl + "pessoa/lista-com-enderecos?idPessoa={id}";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         ListaComEnderecoDTO[] res = given()
                 .contentType(ContentType.JSON)
@@ -133,7 +131,7 @@ public class PessoaService {
 
     public ListaComContatoDTO[] listarListaComContato(String id){
         String url = baseUrl + "pessoa/lista-com-contatos?idPessoa={id}";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         ListaComContatoDTO[] res = given()
                 .contentType(ContentType.JSON)
@@ -155,7 +153,7 @@ public class PessoaService {
 //        String url = baseUrl + "pessoa/data-nascimento?data={data}&dtFinal={dataFinal}";
 //        String data = "01/01/2000";
 //        String dataFinal = "31/12/2022";
-//        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+//        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";  String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
 //
 //        PessoaDTO[] res = given()
 //                .contentType(ContentType.JSON)
@@ -176,7 +174,7 @@ public class PessoaService {
 
     public PessoaDTO[] listarByName(String nome){
         String url = baseUrl + "pessoa/byname?nome={nome}";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         PessoaDTO[] res = given()
                 .contentType(ContentType.JSON)
@@ -196,7 +194,7 @@ public class PessoaService {
 
     public PessoaDTO adicionarPessoa(String jsonBody){
         String url = baseUrl + "pessoa";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         PessoaDTO res = given()
                 .contentType(ContentType.JSON)
@@ -216,7 +214,7 @@ public class PessoaService {
 
     public Erro400DTO adicionarPessoaErro(String jsonBody){
         String url = baseUrl + "pessoa";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         Erro400DTO res = given()
                 .contentType(ContentType.JSON)
@@ -236,7 +234,7 @@ public class PessoaService {
 
     public PessoaDTO atualizarPessoa(String jsonBody,String id){
         String url = baseUrl + "pessoa/{id}";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         PessoaDTO res = given()
                 .contentType(ContentType.JSON)
@@ -257,7 +255,7 @@ public class PessoaService {
 
     public Erro400DTO atualizarPessoaErro(String jsonBody, String id){
         String url = baseUrl + "pessoa/{id}";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         Erro400DTO res = given()
                 .contentType(ContentType.JSON)
@@ -278,7 +276,7 @@ public class PessoaService {
 
     public void deletarPessoa(String id){
         String url = baseUrl + "pessoa/{id}";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         given()
                 .contentType(ContentType.JSON)
@@ -296,7 +294,7 @@ public class PessoaService {
 
     public Erro404DTO deletarPessoaErro(String id){
         String url = baseUrl + "pessoa/{id}";
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5Nzk5NDYxLCJleHAiOjE2NTk4ODU4NjF9.0ARNPOb3vsvH817Ur-mv9VOCPUlzTFaOMCuO0M-4SPU";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2ZW1zZXItYXBpIiwianRpIjoiNDA0IiwiY2FyZ29zIjpbXSwiaWF0IjoxNjU5OTc0ODg4LCJleHAiOjE2NjAwNjEyODh9.Ql5a4H2PmDOWNezwJz-iDVueke5xpgb0uCkFWbcJN_0";
 
         Erro404DTO res = given()
                 .contentType(ContentType.JSON)
